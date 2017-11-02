6623393b9d99517415fbcaeede0d47cb1f333b3b
lilyxfiore <liliana.fiorela@gmail.com>
2017-11-02 17:24:38 -0500

    comp-header.js

--- build/index.js
+++ build/index.js
@@ -4,14 +4,14 @@ const Header = (props) =>{
         <header className={props.class}>
             <ul>
                 <li>{props.title}</li>
-                <li><i className={props.search} aria-hidden="true"></i></li>
-                <li><i className={props.ellipsis} aria-hidden="true"></i></li>
+                <li className={props.direction}><i className={props.ellipsis} aria-hidden="true"></i></li>
+                <li className={props.direction}><i className={props.search} aria-hidden="true"></i></li>
             </ul>
         </header>
     )
 }
 
 ReactDOM.render(
-    <Header title="WhatsApp" class="header" search="fa fa-search right" ellipsis="fa fa-ellipsis-v right"/>,
+    <Header title="WhatsApp" class="header" search="fa fa-search" ellipsis="fa fa-ellipsis-v" direction="right"/>,
     document.getElementById('root')
 );
