2696265b5b88d8db07ad41821dcb47f901101a96
lilyxfiore <liliana.fiorela@gmail.com>
2017-11-08 17:58:52 -0500

    user-random

--- app/reducers/ContactsReducer.js
+++ app/reducers/ContactsReducer.js
@@ -1,3 +1,19 @@
+const getJSON = (url, callback) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+        const status = xhr.status;
+        (status === 200) ? callback(null, xhr.response) : callback(status, xhr.response);
+    };
+    xhr.send();
+};
+
+let CONTACTS = [ {name: 'Pedro Perez', img: 'cat.jpg', date: 'Ayer'},
+    {name: 'Marta Perez', img: 'cat.jpg', date: 'Doming'},
+    {name: 'Ana Perez', img: 'cat.jpg', date: '24/10/17'}];
+
+/*
 const CONTACTS = [
     {name: 'Pedro Perez', img: 'cat.jpg', date: 'Ayer'},
     {name: 'Marta Perez', img: 'cat.jpg', date: 'Doming'},
@@ -9,9 +25,16 @@ const CONTACTS = [
     {name: 'Sara Perez4', img: 'cat.jpg', date: 'Domingo'},
     {name: 'Sara Perez6', img: 'cat.jpg', date: 'Domingo'},
     {name: 'Pablo Perez5', img: 'cat.jpg', date: 'Lunes'}
-]
+];
+*/
 
-export default (state = CONTACTS, action) => {
+getJSON('https://randomuser.me/api/?results=10&nat=es',
+    (err, data) => {
+        (err !== null) ? console.log('Something went wrong: ' + err) : data.results.map(e => CONTACTS.push({ name: e.name.first + ' ' + e.name.last, img: e.picture.thumbnail, date: e.location.city }));
+});
+
+
+export default ( state = CONTACTS, action) => {
     switch (action.type) {
         default:
             return state
