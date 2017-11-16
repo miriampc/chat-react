c18bfaa1bd0bc54d740f09296ba519d447b08a24
lilyxfiore <liliana.fiorela@gmail.com>
2017-11-16 11:35:43 -0500

    Updates

--- app/actions/types.js
+++
@@ -1,1 +0,0 @@
-export const CONTACT_SELECTED = 'CONTACT_SELECTED'
--- app/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import ContactsReducer from './ContactsReducer'
-import ContactSelectedReducer from './ContactSelectedReducer'
-
-export default combineReducers({
-    contacts: ContactsReducer,
-    contactSelected: ContactSelectedReducer
-})
