d0980c418fb483f2d326cd37c8b0fd27093adcb8
miriampc <miiriampc@gmail.com>
2017-11-16 02:12:26 -0500

    add class input message

--- app/components/PanelRight/InputMessage.js
+++ app/components/PanelRight/InputMessage.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const InputMessage = () => {
-    return (
-        <div className="footer-panel">
-            <img src="face.png" className="avatar"/>
-            <input id="" type="text" placeholder="Type a message" />
-            <img src="microfono.png" className=""/>
-        </div>
-    );
+class InputMessage extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {value: ''};    
+        this.eventControl = this.eventControl.bind(this);
+    }
+
+    eventControl(event) {
+        if(event.keyCode == 13){
+            alert('Adding....');
+            this.setState({value: event.target.value});
+        }        
+    }
+    render(){
+        return (
+            <div className="footer-panel">
+                <img src="face.png" className="avatar"/>
+                <input id="message-text" type="text" placeholder="Type a message" />
+                <input
+                    type="text"
+                    placeholder="Type a message"
+                    value={this.state.value}
+                    onChange={this.eventControl}
+                />
+                <img src="microfono.png" className=""/>
+            </div>
+        )
+    }    
 }
+//const InputMessageWithRedux = connect()
 
 export default InputMessage
--- app/components/PanelRight/Messages.js
+++ app/components/PanelRight/Messages.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import InputMessage from "./InputMessage"
 
 const Messages = () => {
     return (
